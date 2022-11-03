fab3f4ce2efbed327624412a456ee20aab8926cc
AlexDeev1996 <shtormmc@gmail.com>
2022-11-03 21:18:23 +0100

    add new form

--- language_aplication/language_aplication/Form1.cs
+++ language_aplication/language_aplication/Form1.cs
@@ -1,10 +1,22 @@
+﻿using Excel = Microsoft.Office.Interop.Excel;
+using System.Runtime.InteropServices;
+
 namespace language_aplication
 {
     public partial class Form1 : Form
     {
+        private Excel.Application excelapp = null;
+        private Excel.Window excelWindow = null;
+
         public Form1()
         {
             InitializeComponent();
+
+            string path_word = @"C:\Users\Alex\source\repos\language_aplication\appDE.xlsx";
+          
+
+
+            
         }
     }
 }
